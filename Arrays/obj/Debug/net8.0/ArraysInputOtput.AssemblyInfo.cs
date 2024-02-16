4c485208fc8b4ba040ffe0295216399e01f9b722
Silviya16st <silviyas225@gmail.com>
2024-02-16 13:15:31 +0200

    second commit

--- Arrays/obj/Debug/net8.0/ArraysInputOtput.AssemblyInfo.cs
+++ Arrays/obj/Debug/net8.0/ArraysInputOtput.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArraysInputOtput")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b175cb7c13aac103a0a9bb30264bea8e9728dadb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArraysInputOtput")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArraysInputOtput")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ SredenElement/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SredenElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vavedi chisla: ");
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int middle = numbers.Length / 2;
+           
+            Console.WriteLine(numbers[middle]);
+            
+        }
+    }
+}
