b175cb7c13aac103a0a9bb30264bea8e9728dadb
Silviya16st <silviyas225@gmail.com>
2024-02-16 12:31:04 +0200

    vtora proba

---
+++ AgregatArrey/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace AgregatArrey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Dai broi na elementite: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+            //inpu the elements
+            for (int i = 0; i <= numbers.Length-1; i++)
+            {
+                Console.Write($"num[{1}] = ");
+                numbers[1] = int.Parse(Console.ReadLine());
+            }
+
+            //2
+            long sum = 0l;
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                sum = sum + numbers[i];
+                
+            }
+            Console.WriteLine($"Sum= {sum}");
+
+            int min = int.MinValue; 
+            int minPos = 0;
+            for(int i = 0 ; i < numbers.Length ; i++)
+            {
+                if (numbers[i] < min)
+                {
+                    min = numbers[i];
+                    minPos = i;
+                }
+            }
+
+            //print ALL elements
+            for (int i = 0;i <= numbers.Length-1; i++)
+            {
+                Console.WriteLine($"num[{1}] = {numbers[i]}");
+            }
+        }
+    }
+}
---
+++ Arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Arrays_InputOtput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //Deklariram masiv ot 13 elementa
+            double[] grades = new double[13]; //{ 1, 2, 3, 4};
+            string[] names = new string[13] { "Lubo", "Milen", "Mihail",
+                "Radoslav", "Svetlomir", "Siana", "Silviya", "Stanislav", 
+                "Tedo", "Hristian", "Hristomir", "Cvetelina", "Cvetina"};
+            
+            //vavejdat se elementi ot klaviatyrata - cikal
+            for (int index = 0; index < grades.Length; index++)
+            {
+                Console.WriteLine($"Ocenkata na {names[index]}");
+                grades[index] = double.Parse(Console.ReadLine());
+            }
+            //obrabotka
+            grades[11] = 6;
+            names[11]="Cvetelina";
+            grades[11] = Math.Ceiling(5.24);
+            
+            //Otpechatvane na rezyltatite
+            for (int j = 0; j < names.Length; j++)
+            {
+                Console.WriteLine($"{j+14}");
+            }
+        }
+    }
+}
